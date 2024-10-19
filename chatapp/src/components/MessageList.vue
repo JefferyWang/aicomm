fbfa835ba8df8aed0829425a00b59c9c5ec05738
JefferyWang <admin@wangjunfeng.com>
2024-10-19 17:55:44 +0800

    feature: support display and enlarge image.

--- chatapp/src/components/MessageList.vue
+++ chatapp/src/components/MessageList.vue
@@ -5,13 +5,21 @@
     </div>
     <div v-else>
       <div v-for="message in messages" :key="message.id" class="flex items-start mb-5">
-        <img :src="`https://ui-avatars.com/api/?name=${getSender(message.senderId).fullname.replace(' ', '+')}`" class="w-10 h-10 rounded-full mr-3" alt="Avatar" />
+        <img :src="`https://ui-avatars.com/api/?name=${getSender(message.senderId).fullname.replace(' ', '+')}`"
+          class="w-10 h-10 rounded-full mr-3" alt="Avatar" />
         <div class="max-w-4/5">
           <div class="flex items-center mb-1">
             <span class="font-bold mr-2">{{ getSender(message.senderId).fullname }}</span>
             <span class="text-xs text-gray-500">{{ message.formattedCreatedAt }}</span>
           </div>
           <div class="text-sm leading-relaxed break-words whitespace-pre-wrap">{{ message.content }}</div>
+          <div v-if="message.files && message.files.length > 0" class="grid grid-cols-3 gap-2 mt-2">
+            <div v-for="(file, index) in message.files" :key="index" class="relative">
+              <img :src="getFileUrl(file)"
+                :class="{ 'h-32 object-cover cursor-pointer': true, 'w-auto h-auto': enlargedImage[message.id] }"
+                @click="toggleImage(message.id)" alt="Uploaded file" />
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -19,7 +27,14 @@
 </template>
 
 <script>
+import { getUrlBase } from '../utils';
+
 export default {
+  data() {
+    return {
+      enlargedImage: {},
+    };
+  },
   computed: {
     messages() {
       return this.$store.getters.getMessagesForActiveChannel;
@@ -59,6 +74,13 @@ export default {
       if (container) {
         container.scrollTop = container.scrollHeight;
       }
+    },
+    getFileUrl(file) {
+      return `${getUrlBase()}${file}?token=${this.$store.state.token}`;
+    },
+    toggleImage(messageId) {
+      this.enlargedImage[messageId] = !this.enlargedImage[messageId];
+      this.enlargedImage = { ...this.enlargedImage };
     }
   },
   mounted() {
